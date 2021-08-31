251b7da14baeffbc275caca7c809891ff7ce5bcb
Peter Shen <shenjiashu@hotmail.com>
2021-08-31 12:55:06 -0700

    initialized hashmap and added tester

--- src/SimpleATM.java
+++ src/SimpleATM.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class SimpleATM {
 
-	private static HashMap<String, Double> accounts;
-
-	public static void deposit(String ID, double money)
+	private static HashMap<String, Double> accounts = new HashMap<String, Double>();
+	
+	public void deposit(String ID, double money)
 	{
 		if (money > 0)
 		{
@@ -14,7 +14,7 @@ public class SimpleATM {
 		}
 	}
 	
-	public static void withdraw(String ID, double money)
+	public void withdraw(String ID, double money)
 	{
 		if (accounts.containsKey(ID))
 		{
@@ -25,7 +25,7 @@ public class SimpleATM {
 		}
 	}
 	
-	public static String checkBalance(String ID)
+	public String checkBalance(String ID)
 	{
 		String end = "";
 		if (accounts.containsKey(ID))
@@ -36,6 +36,21 @@ public class SimpleATM {
 		{
 			return "ID does not exist.";
 		}
+	}
+	
+	public static void main (String [] args)
+	{
+		SimpleATM thing = new SimpleATM();
+		thing.deposit("Winfrey", 25159684372.43);
+		thing.deposit("Bezos", 193495330293.43);
+		thing.deposit("Bezos", 20394353.35);
+		thing.withdraw("Bezos", 3593293.66);
+		thing.deposit("MrTheiss",64033.55);
+		thing.withdraw("MrTheiss", 443964.46);
+		System.out.println(thing.checkBalance("Bezos"));
+		System.out.println(thing.checkBalance("Winfrey"));
+		System.out.println(thing.checkBalance("YoMama"));
+		System.out.println(thing.checkBalance("MrTheiss"));
 	}
 
 }
