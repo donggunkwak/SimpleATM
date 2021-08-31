18aa10dbeb342b73014b280412c3ffcda7a74670
Peter Shen <shenjiashu@hotmail.com>
2021-08-31 12:38:10 -0700

    added withdraw, changed money to double

--- src/SimpleATM.java
+++ src/SimpleATM.java
@@ -2,17 +2,27 @@ import java.util.*;
 
 public class SimpleATM {
 
-	private static HashMap<String, Integer> accounts;
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-	}
-	public static void deposit(String ID, int money)
+	private static HashMap<String, Double> accounts;
+
+	public static void deposit(String ID, double money)
 	{
-		if(!accounts.containsKey(ID))
-			accounts.put(ID,0);
-		accounts.put(ID,accounts.get(ID)+money);
+		if (money > 0)
+		{
+			if(!accounts.containsKey(ID))
+				accounts.put(ID,0.0);
+			accounts.put(ID,(accounts.get(ID)+money));
+		}
+	}
+	
+	public static void withdraw(String ID, double money)
+	{
+		if (accounts.containsKey(ID))
+		{
+			if (money > 0 && money < accounts.get(ID))
+			{
+				accounts.replace(ID, accounts.get(ID)-money);
+			}
+		}
 	}
 	
 
